0655e7e6e41ff43a1dd34ebe130f3989ac41c3a6
VovaSai <volodymyr.sai.ki.2023@lpnu.ua>
2025-12-18 00:41:08 +0200

    Add files via upload

---
+++ lab7/main.py
@@ -0,0 +1,44 @@
+def lengthOfEachSubArray(size):
+    arr = [None] * size
+    mid = size // 2
+
+    for i in range(size):
+        length = 0
+        for j in range(size):
+            if (i + j < mid) or (j - i > mid) or (i - j > mid) or (j + i > (3 * mid)):
+                length += 1
+        arr[i] = [""] * length
+
+    return arr
+
+def printMatrix(arr, symbol, size, file):
+    print("Результат матриці")
+
+    with open(file, "w", encoding="utf-8") as writer:
+        mid = size // 2
+
+        for i in range(size):
+            index_j = 0
+            for j in range(size):
+                if (i + j < mid) or (j - i > mid) or (i - j > mid) or (j + i > (3 * mid)):
+                    arr[i][index_j] = symbol
+                    writer.write(arr[i][index_j] + " ")
+                    print(arr[i][index_j], end=" ")
+                    index_j += 1
+                else:
+                    writer.write("  ")
+                    print("  ", end="")
+            print()
+            writer.write("\n")
+        writer.flush()
+
+if __name__ == '__main__':
+    size = int(input("Введіть розмір матриці: "))
+    symbol = input("Введіть символ заповнювач: ")
+
+    if len(symbol) != 1:
+        print("Введіть коректний символ заповнювач")
+    else:
+        arr = lengthOfEachSubArray(size)
+        file_name = "matrix_output.txt"
+        printMatrix(arr, symbol, size, file_name)
